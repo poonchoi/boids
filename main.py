04c187778992b2e6f6947dc9395deefed653f8ff
dan <80271815+poonchoi@users.noreply.github.com>
2022-02-23 21:30:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,65 @@
+from math import radians
+from tkinter import W, Y
 import pygame
 import numpy
-
-# # pygame initialize
-# pygame.init()
-
-# dimensions = (1920,1080)
-# width = dimensions[0]//2
-# height = dimensions[1]//2
-
-# screen = pygame.display.set_mode(dimensions)
-# clock = pygame.time.Clock()
-# fps = 75
-
-# screen.fill((0,0,0))
-
-# class boid():
-#     def __init__(self, position, velocity, size, color):
-#         self.position = [0,0]
-#         self.velocity = [3,1]
-#         self.size = 1
-#         self.color = (255,255,255)
-
-#     def move(self):
-#         self.position = [(self.velocity[0]+self.position[0]), (self.velocity[1]+self.position[1])]
-        
-#     def check_collision(self):
-#         if 0 < self.position[0] < 1080:
-#             self.position[1] *= -1
-#         if 0 < self.position[1] < 1920:
-#             self.position[0] *= -1
-
-#     def draw(self):
-#         x = self.position[0]
-#         y = self.position[1]
-#         coords = (x, y)
-#         pygame.draw.circle(screen, self.color, coords, self.size)
-
-
-# population = 1
-# p = [boid([0,0], [3,1], 5, (255,255,255)) for i in range(population)]
-
-# run = True
-
-# while run:
-#     clock.tick(fps)
-#     for i in pygame.event.get():
-#         if i.type == pygame.QUIT:
-#             run = False
-#     screen.fill((0,0,0))
-
-#     for i in p:
-#         i.check_collision()
-#         i.move()
-#         i.draw()
+import random
 
 # pygame initialize
 pygame.init()
 
-dimensions = (500,500)
-width = dimensions[0]//2
-height = dimensions[1]//2
+dimensions = (1920,1080)
+width = dimensions[0]
+height = dimensions[1]
 
 screen = pygame.display.set_mode(dimensions)
 clock = pygame.time.Clock()
 fps = 75
 
 screen.fill((0,0,0))
+
+class boid():
+    def __init__(self, position, velocity, size, color):
+        self.position = position
+        self.velocity = velocity
+        self.size = size
+        self.color = color
+        self.radius = self.size//2
+
+    def move(self):
+        self.position = [(self.velocity[0]+self.position[0]), (self.velocity[1]+self.position[1])]
+        
+    def check_collision(self):
+        xpos = self.position[0]
+        ypos = self.position[1]
+        xvel = self.velocity[0]
+        yvel = self.velocity[1]
+        radius = self.size//2
+
+        if (xpos <= 0+radius) or (xpos >= width-radius):
+            xvel *= -1
+        if (ypos <= 0+radius) or (ypos >= height-radius):
+            yvel *= -1
+        
+        self.velocity = [xvel, yvel]
+
+    def draw(self):
+        x = self.position[0]
+        y = self.position[1]
+        coords = (x, y)
+        pygame.draw.circle(screen, self.color, coords, self.size)
+        pygame.display.update()
+
+
+def spawn():
+    x = random.randint(0, width)
+    y = random.randint(0, height)
+    vx = random.randint(-5, 5)
+    vy = random.randint(-5, 5)
+    return x, y, vx, vy
+
+population = 10
+
+p = [boid([random.randint(0,width),random.randint(0,height)],[random.randint(-2,2),random.randint(-2,2)],7,(255,255,255)) for i in range(population)]
 
 run = True
 
@@ -74,7 +68,9 @@ while run:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
+    screen.fill((0,0,0))
 
-    pygame.draw.circle(screen, (255,255,255), (10,10), 7)
-
-pygame.quit()
+    for i in p:
+        i.check_collision()
+        i.move()
+        i.draw()
