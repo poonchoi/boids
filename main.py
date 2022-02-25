540c8cd5063cc1b4c7dfed6b55d37a68763ba22c
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-02-25 23:21:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from cv2 import compare
 import pygame
 import random
+import ctypes
+import math
+
+from pyparsing import White
 
 # pygame initialize
 pygame.init()
 
-dimensions = (1920,1080)
+user32 = ctypes.windll.user32
+dimensions = (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
 width = dimensions[0]
 height = dimensions[1]
 
@@ -14,6 +20,13 @@ fps = 75
 
 screen.fill((0,0,0))
 
+##colors##
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+white = (255,255,255)
+##########
+
 class boid():
     def __init__(self, position, velocity, size, color):
         self.position = position
@@ -21,42 +34,80 @@ class boid():
         self.size = size
         self.color = color
         self.radius = self.size//2
+    
+    def __getitem__(self, item):
+        return getattr(self, item)
 
     def move(self):
         self.position = [(self.velocity[0]+self.position[0]), (self.velocity[1]+self.position[1])]
         
-    def check_collision(self):
+    # def check_wall_collision(self):
+    #     xpos = self.position[0]
+    #     ypos = self.position[1]
+    #     xvel = self.velocity[0]
+    #     yvel = self.velocity[1]
+    #     radius = self.size//2
+
+    #     if (xpos <= 0+radius) or (xpos >= width-radius):
+    #         xvel *= -1
+    #     if (ypos <= 0+radius) or (ypos >= height-radius):
+    #         yvel *= -1
+
+    #     self.velocity = [xvel, yvel]
+
+    def check_wall_collision(self):
         xpos = self.position[0]
         ypos = self.position[1]
         xvel = self.velocity[0]
         yvel = self.velocity[1]
         radius = self.size//2
 
-        if (xpos <= 0+radius) or (xpos >= width-radius):
-            xvel *= -1
-        if (ypos <= 0+radius) or (ypos >= height-radius):
-            yvel *= -1
-        
-        self.velocity = [xvel, yvel]
+        if xpos < 0+radius:
+            xpos = width-radius
+        if xpos > width-radius:
+            xpos = (0+radius)
+        if ypos < 0+radius:
+            ypos = height-radius
+        if ypos > height-radius:
+            ypos = (0+radius)
+
+        self.position = [xpos,ypos]
 
     def draw(self):
         x = self.position[0]
         y = self.position[1]
         coords = (x, y)
         pygame.draw.circle(screen, self.color, coords, self.size)
-
+    
+    def steer(self, all_boids):
+        currentboid = self.position
+        for i in all_boids:
+            compareboid = i['position']
+            if math.sqrt(((currentboid[0]-compareboid[0])**2)+((currentboid[1]-compareboid[1])**2)) < 100:
+                pygame.draw.line(screen, white, currentboid, compareboid, 2)
+                # self.velocity = i['velocity']
 
 def spawn():
-    vels = [5, 4, 3, 2, 1, -1, -2, -3, -4, -5]
-    x = random.randint(1, width-1)
-    y = random.randint(1, height-1)
+    x = random.randint(20, width-20)
+    y = random.randint(20, height-20)
+    return [x,y]
+
+def startvel():
+    a = -3
+    vels = []
+
+    for i in range(1, a*-2):
+        a+=1
+        if a != 0:
+            vels.append(a)
+
     vx = random.choice(vels)
     vy = random.choice(vels)
-    return x, y, vx, vy
+    return [vx,vy]
 
-population = 1000
+population = 100
 
-p = [boid([random.randint(0,width),random.randint(0,height)],[random.randint(-2,2),random.randint(-2,2)],7,(255,255,255)) for i in range(population)]
+p = [boid(spawn(), startvel(), 5, red) for i in range(population)]
 
 run = True
 
@@ -67,8 +118,11 @@ while run:
             run = False
     screen.fill((0,0,0))
 
-    for i in p:
-        i.check_collision()
-        i.move()
-        i.draw()
+    for i in range(len(p)):
+        p[i].check_wall_collision()
+        p[i].move()
+        p[i].steer(p)
+        p[i].draw()
+        
+
     pygame.display.update()
